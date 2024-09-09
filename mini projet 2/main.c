48d8df40592053b4553f08200665e1d45e5856eb
HAMZA <jaafar.hamza711@gmail.com>
2024-09-09 03:10:44 +0100

    Add files via upload

---
+++ mini projet 2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Create a structure called myStructure
+struct myStructure {
+  int myNum;
+  char myLetter;
+};
+
+int main() {
+  // Create a structure variable of myStructure called s1
+  struct myStructure s1;
+
+  // Assign values to members of s1
+  //s1.myNum = 13;
+  //s1.myLetter = 'B';
+
+  // Print values
+  printf("my Num :");
+  scanf("%d",&s1.myNum);
+  printf("My letter:");
+  scanf("%s",&s1.myLetter);
+  return 0;
+}
